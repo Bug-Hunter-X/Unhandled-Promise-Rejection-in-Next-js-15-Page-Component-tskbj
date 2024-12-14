f27aaf29807bf7458e84aef95f653b6fc88ae16b
BugHunter-X <askzenai@gmail.com>
2024-12-14 22:42:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,31 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error in Next.js 15 if not handled properly
+  const unhandledPromise = new Promise((resolve, reject) => {
+      reject(new Error('Unhandled promise rejection'));
+  });
+
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+```
